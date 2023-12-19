283a223f467544c5fbf212438689a9523173ce5c
Eduardo Samaniego <75852807+imyuru@users.noreply.github.com>
2023-12-19 14:01:51 -0500

    Add files via upload

---
+++ SisInve/includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', '192.168.0.23' );          // Set database host
+  define( 'DB_USER', 'apacheuser' );             // Set database user
+  define( 'DB_PASS', 'granyayo1' );             // Set database password
+  define( 'DB_NAME', 'dbInventario' );        // Set database name
+
+?>
